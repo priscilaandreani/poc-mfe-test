f1dd728f7b4ec49756f8daf16a380ace181b34d7
Priscila Andreani <priscila.andreani@grupoboticario.com.br>
2023-06-22 12:43:22 -0300

    chore: create react webapp federated

---
+++ federated/webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+
+module.exports = {
+  entry: './src/index',
+  mode: 'development',
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: 'http://localhost:9001/'
+  },
+  devServer:{ 
+    port: 9001,
+    historyApiFallback: true,
+    static: {
+      directory: path.resolve(__dirname, "static"),
+    }
+  },
+  resolve: {
+    extensions: ['.jsx', '.js', '.json'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: require.resolve('babel-loader'),
+        options: {
+          presets: [require.resolve('@babel/preset-react')]
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './public/index.html',
+      title: 'Consumer'
+    }),
+    new ModuleFederationPlugin({
+      name: 'Federated',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './Button': './src/App'
+      }
+    })
+  ]
+}
